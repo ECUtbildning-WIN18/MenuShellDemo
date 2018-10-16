71eb2f5872b16724829c33bba9741f2109829f1a
Jonathan Moller <jonathanmoller@liver.se>
2018-10-16 10:39:49 +0200

    Add project files.

---
+++ MenuShellDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuShellDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
